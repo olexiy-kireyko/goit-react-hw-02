917b0dd148e9597657917dc18b3d761dd9495dc6
olexiy-kireyko <a.kireyko@gmail.com>
2024-12-12 14:47:34 +0200

    hw1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-// import { useState } from 'react';
-
 import { useEffect, useState } from 'react';
 import './App.css';
 import Description from './components/Description/Description';
@@ -8,29 +6,21 @@ import Options from './components/Options/Options';
 import Notification from './components/Notification/Notification';
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const memoryFeedback = window.localStorage.getItem('memoryFeedback');
+    if (memoryFeedback !== null) {
+      return JSON.parse(memoryFeedback);
+    }
+    return { good: 0, neutral: 0, bad: 0 };
   });
 
-  const [totalFeedback, setTotalFeedback] = useState(0);
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
   const updateFeedback = feedbackType => {
     setFeedback({
       ...feedback,
       [feedbackType]: feedback[feedbackType] + 1,
     });
-
-    setTotalFeedback(prev => prev + 1);
-    // console.log('totalFeedback', totalFeedback);
-    // console.log('feedback.good', feedback.good);
-    // console.log('feedback.neutral', feedback.neutral);
-    // console.log('feedback.bad', feedback.bad);
-    // console.log(
-    //   'feedback.good +feedback.neutral + feedback.bad',
-    //   feedback.good + feedback.neutral + feedback.bad
-    // );
   };
 
   const resetFeedback = () => {
@@ -39,8 +29,11 @@ function App() {
       neutral: 0,
       bad: 0,
     });
-    setTotalFeedback(0);
   };
+
+  useEffect(() => {
+    window.localStorage.setItem('memoryFeedback', JSON.stringify(feedback));
+  }, [feedback]);
 
   return (
     <>
@@ -52,11 +45,7 @@ function App() {
       />
 
       {totalFeedback ? (
-        <Feedback
-          value={feedback}
-          // updateFeedback={updateFeedback}
-          totalFeedback={totalFeedback}
-        />
+        <Feedback value={feedback} totalFeedback={totalFeedback} />
       ) : (
         <Notification />
       )}
